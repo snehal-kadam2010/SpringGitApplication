dad8edbac9ad35e034546235aa6ef929fe466a32
snehal-kadam2010 <107234210+snehal-kadam2010@users.noreply.github.com>
2023-03-21 14:34:25 +0530

    [STNO-2] "ADD THE CUSTOME DATA"

--- springGit/src/main/java/com/csi/controller/CustomeController.java
+++ springGit/src/main/java/com/csi/controller/CustomeController.java
@@ -1,4 +1,32 @@
 package com.csi.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+ class Vehicle{
+  private  int custId;
+  private  String custName;
+  private double custSalary;
+
+}
+@RestController
+@RequestMapping("/v1")
 public class CustomeController {
+ @GetMapping("/vehical")
+ public ResponseEntity<List<Vehicle>> getData(){
+  return ResponseEntity.ok(Stream.of(new Vehicle(11,"swara",45000),new Vehicle(12,"ram",58000)).collect(Collectors.toList()));
+ }
 }
